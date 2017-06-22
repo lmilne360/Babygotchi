d135c63ac4c564e10565bb4901925b50945dfd53
Lmilne <lmilne360@gmail.com>
2017-06-21 22:27:03 -0400

    Added angular material library and flex layout to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BabiesComponent } from './babies/babies.component';
 
+/* Material Design and Flex Layout */
+
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+
 //Angular Fire
 import { AngularFireModule } from 'angularfire2';
-import{ AngularFireDatabaseModule} from 'angularfire2/database'
+import { AngularFireDatabaseModule} from 'angularfire2/database'
+import { AngularFireAuthModule } from 'angularfire2/auth'
 
 //Firebase config file
 import { environment } from 'environments/environment';
+
+/* Services */
 import { RandomPickerService } from './random-picker.service';
 
 @NgModule({
@@ -21,7 +31,11 @@ import { RandomPickerService } from './random-picker.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
     AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
     AngularFireDatabaseModule
   ],
   providers: [RandomPickerService],
