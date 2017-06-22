9a04525843a1b25f7bdb324be3497ba96a21c284
Lmilne <lmilne360@gmail.com>
2017-06-21 21:05:53 -0400

    Setup firebase baby observable data stream

--- src/app/babies/babies.component.ts
+++ src/app/babies/babies.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Baby } from 'app/baby';
+
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+
 @Component({
   selector: 'app-babies',
   templateUrl: './babies.component.html',
   styleUrls: ['./babies.component.scss']
 })
 export class BabiesComponent implements OnInit {
-  babies = [new Baby('Lola'), new Baby('Toby')];
-  constructor() { }
+  babies: FirebaseListObservable<Baby[]>;
+
+  constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
+  	this.babies = this.db.list('/babies');
+  }
+
+  //create new baby
+  giveBirth(){
+  	const newBaby = new Baby('Ali')
+  	const babies = this.db.list('/babies')
+  	// this saves the baby 
+  	babies.push(newBaby)
   }
 
 }
