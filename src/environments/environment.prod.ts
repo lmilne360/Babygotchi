0e6a8bd3cac2e154211a4560284ea797fa87dde0
Lmilne <lmilne360@gmail.com>
2017-06-22 04:56:25 -0400

    production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyBNLWQJi5q9ZEPQ4DNEwHwHh9S-uyOVoNI",
+    authDomain: "babygotchi-4eafd.firebaseapp.com",
+    databaseURL: "https://babygotchi-4eafd.firebaseio.com",
+    projectId: "babygotchi-4eafd",
+    storageBucket: "",
+    messagingSenderId: "950195300452"
+  }
 };
