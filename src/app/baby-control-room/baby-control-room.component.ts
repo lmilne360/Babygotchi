4dd41a1a777611e21bcdd6b14d762322f4f199f8
Lmilne <lmilne360@gmail.com>
2017-09-26 21:00:18 -0400

    Adjust baby control room

--- src/app/baby-control-room/baby-control-room.component.ts
+++ src/app/baby-control-room/baby-control-room.component.ts
@@ -7,7 +7,7 @@ import { MdSnackBar } from '@angular/material';
 import { RandomPickerService } from 'app/random-picker.service';
 
 const messages = ['You are evil!', 'I bow before you Dr Evil!', 'Satanaaaas!!', 'Keepin\' it real!', 'Wow that was mean!',
-                  'You have no shame!', 'OMG reaaaallllly?' ];
+  'You have no shame!', 'OMG reaaaallllly?'];
 
 @Component({
   selector: 'app-baby-control-room',
@@ -18,45 +18,49 @@ export class BabyControlRoomComponent implements OnInit {
   babyId: string;
 
   constructor(private activatedRoute: ActivatedRoute,
-              private firebaseApp: FirebaseApp,
-              private mdSnackBar: MdSnackBar,
-              private randomPicker: RandomPickerService) { }
+    private firebaseApp: FirebaseApp,
+    private mdSnackBar: MdSnackBar,
+    private randomPicker: RandomPickerService) { }
 
   ngOnInit() {
     this.activatedRoute
-        .parent
-        .params
-        .subscribe(
-          p => this.babyId = p['id']
-        );
+      .parent
+      .params
+      .subscribe(
+      p => this.babyId = p['id']
+      );
+  }
+
+  adjustLife() {
+    const babeRef = this.firebaseApp.database().ref(`/babies/${this.babyId}`);
+    babeRef.transaction(baby => {
+      baby.life = (100 - Math.round((baby.hunger + baby.shittiness + baby.sleepiness) / 3));
+      return baby;
+    });
   }
 
   babyHungry() {
     this.showMessage();
     const statRef = this.firebaseApp.database().ref(`/babies/${this.babyId}/hunger`);
     statRef.transaction(stat => stat + 10 > 100 ? stat : stat + 10);
+    this.adjustLife();
   }
 
   babyShitty() {
     this.showMessage();
     const statRef = this.firebaseApp.database().ref(`/babies/${this.babyId}/shittiness`);
     statRef.transaction(stat => stat + 10 > 100 ? stat : stat + 10);
+    this.adjustLife();
   }
 
   babySleepy() {
     this.showMessage();
     const statRef = this.firebaseApp.database().ref(`/babies/${this.babyId}/sleepiness`);
     statRef.transaction(stat => stat + 10 > 100 ? stat : stat + 10);
+    this.adjustLife();
   }
 
-  /*
-  adjustLife() {
-    const statRef = this.firebaseApp.database().ref(`/babies/${this.babyId}/life`);
-    statRef.transaction(stat => updatedLife)
-  }
-
-  */
   showMessage() {
-    this.mdSnackBar.open(this.randomPicker.pickAtRandom(messages), null, {duration: 3000});
+    this.mdSnackBar.open(this.randomPicker.pickAtRandom(messages), null, { duration: 3000 });
   }
 }
